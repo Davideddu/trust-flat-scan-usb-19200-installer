76d2b67555d08be45af06c94d8693689b6e59235
Davide Depau <me@davideddu.org>
2016-03-04 00:25:55 +0100

    use urllib2 instead of wget to obtain firmware

--- install.py
+++ install.py
@@ -13,13 +13,13 @@ vendor "Trust"
 model "Flat Scan USB 19200" """[:-1]
 
 import os, sys
-import re, shutil
+import re, urllib2
 import subprocess as sp
 
 if os.geteuid() > 0:
     raise SystemExit("Please run this program as root.")
 
-dpkg = sp.Popen(["dpkg-query", "-W", "--showformat='${Status}\n'", "xine-dbg"], stdout=sp.PIPE)
+dpkg = sp.Popen(["dpkg-query", "-W", "--showformat='${Status}\n'", "sane"], stdout=sp.PIPE)
 out = dpkg.communicate()[0]
 
 if "install ok installed" not in out:
@@ -31,35 +31,40 @@ if "install ok installed" not in out:
         raise SystemExit("Installation seems to have failed. Please try installing it manually by running \"sudo apt-get install sane\", then try again.")
 
 print "Sane is installed"
-print "Downloading and installing scanner firmware..."
-
-wget = sp.Popen(["wget", "https://raw.githubusercontent.com/Davideddu/trust-flat-scan-usb-19200-installer/master/GT680XFW.USB", "-O", "/tmp/GT680XFW.USB"])
-wget.wait()
-
-if wget.poll() > 0:
-    raise SystemExit("Download failed. Please try again.")
+print "Downloading scanner firmware... ",
 
 try:
     os.makedirs("/usr/share/sane/gt68xx/")
 except OSError:
     pass
 
-shutil.copy("/tmp/GT680XFW.USB", "/usr/share/sane/gt68xx/GT680XFW.USB")
-os.remove("/tmp/GT680XFW.USB")
-os.chmod("/usr/share/sane/gt68xx/GT680XFW.USB", 662)
+try:
+    remote = urllib2.urlopen("https://raw.githubusercontent.com/Davideddu/trust-flat-scan-usb-19200-installer/master/GT680XFW.USB")
+    local = open("/usr/share/sane/gt68xx/GT680XFW.USB", "wb")
+    local.write(remote.read())
+finally:
+    local.close()
+    remote.close()
 
-print "Patching /etc/sane.d/dll.conf"
+os.chmod("/usr/share/sane/gt68xx/GT680XFW.USB", 662)
+print "Done."
+
+print "Patching /etc/sane.d/dll.conf... ",
 with open("/etc/sane.d/dll.conf", "r") as f:
     c = f.read()
+    f.close()
 with open("/etc/sane.d/dll.conf", "w") as f:
     c = re.sub(r"[#]\s*gt68xx", "gt68xx", c)
     f.write(c)
+print "Done."
 
-print "Patching /etc/sane.d/gt68xx.conf"
+print "Patching /etc/sane.d/gt68xx.conf... ",
 with open("/etc/sane.d/gt68xx.conf", "r") as f:
     c = f.read()
+    f.close()
 with open("/etc/sane.d/gt68xx.conf", "w") as f:
     c = re.sub(REGEX, REPL, c)
     f.write(c)
-
+print "Done."
+print
 print "Done. Run \"scanimage --list-devices\" to see if the scanner is detected. You may need to reboot."
